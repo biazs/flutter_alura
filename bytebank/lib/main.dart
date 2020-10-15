2c781365cf855cd1e6915d1f2b8f6c7b4db38081
Bia Zoroastro <fabianazs@gmail.com>
2020-10-15 15:19:09 -0300

    Tela inicial - byteBank - Hot reload

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,58 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: ListaTransferencias(),
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferência'),),
+      body: Text('teste'));
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 1001)),
+          ItemTransferencia(Transferencia(300.0, 2000)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
-    ),
-  ));
-}
-
-class ListaTransferencias extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    
-    return Column(
-        children: <Widget>[
-          ItemTransferencia(Transferencia(100.0, 1000)),          
-          ItemTransferencia(Transferencia(200.0, 1001)), 
-          ItemTransferencia(Transferencia(300.0, 2000)), 
-        ],
-      );
+    );
   }
-
 }
 
-class ItemTransferencia extends StatelessWidget{
-  
+class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(_transferencia.valor.toString()),
-              subtitle: Text(_transferencia.numeroConta.toString()),
-            ),
-          );
-  }  
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
 }
 
 class Transferencia {
   final double valor;
   final int numeroConta;
 
-  Transferencia(this.valor, this.numeroConta);  
+  Transferencia(this.valor, this.numeroConta);
 }
 /*
 class MyApp extends StatelessWidget {
