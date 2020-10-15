68b832d5fc92ca8ec0609fac41b7cd2c4072f91a
Bia Zoroastro <fabianazs@gmail.com>
2020-10-15 14:15:55 -0300

    Tela inicial - byteBank - appBar, floatingActionButton

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Column(
-    children: <Widget>[
-      Text('Deliver features faster', textDirection: TextDirection.ltr,),
-      Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
-      Expanded(
-        child: FittedBox(
-          fit: BoxFit.contain, // otherwise the logo will be tiny
-          child: const FlutterLogo(),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Transferências'),),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
         ),
       ),
-    ],
-  ));
+    )
+  );
 }
 /*
 class MyApp extends StatelessWidget {
