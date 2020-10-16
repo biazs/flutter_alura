d374c496158d8ab70e48cbd3ec2da007f12c0957
Bia Zoroastro <fabianazs@gmail.com>
2020-10-16 17:39:35 -0300

    Implementando a lista de contatos

--- appPersistencia/bytebank/lib/main.dart
+++ appPersistencia/bytebank/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+
+import 'screens/contacts_list.dart';
 
 void main() {
   runApp(BytebankApp());
@@ -19,51 +22,4 @@ class BytebankApp extends StatelessWidget {
       home: Dashboard(),
     );
   }
-}
-
-class Dashboard extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {    
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/bytebank_logo.png'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 90,
-                width: 150,
-                color: Theme.of(context).primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    Text('Contacts',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        )),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-  }
-  
-}
+} 
