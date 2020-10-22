304e48ae6d8a4f2d8462fb5b916d6f3a8217fcde
Bia Zoroastro <fabianazs@gmail.com>
2020-10-22 20:52:07 -0300

    Utilizando interceptador do http

--- appPersistencia/bytebank/lib/http/webclient.dart
+++ appPersistencia/bytebank/lib/http/webclient.dart
@@ -1,6 +1,29 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
-void findAll() async{
-  final Response response = await get('http://192.168.18.25:8080/transactions');
-  print(response.body);
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('======== Request ========');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.headers}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('======== Response ========');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.headers}');
+    return data;
+  }
+}
+
+void findAll() async {
+  final Client client = HttpClientWithInterceptor.build(interceptors: [
+    LoggingInterceptor(),
+  ]);
+  final Response response = await client.get('http://192.168.18.25:8080/transactions');  
 }
