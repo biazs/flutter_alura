96dc033358775506ad136c604f8dc37bac27b934
Bia Zoroastro <fabianazs@gmail.com>
2020-10-26 17:13:22 -0300

    Refatorando o código

--- appPersistencia/bytebank/lib/screens/transaction_form.dart
+++ appPersistencia/bytebank/lib/screens/transaction_form.dart
@@ -89,20 +89,22 @@ class _TransactionFormState extends State<TransactionForm> {
   void _save(Transaction transactionCreated, String password,
       BuildContext context) async {
     //await Future.delayed(Duration(seconds: 1));
-    _webClient.save(transactionCreated, password).then((transaction) {
-      if (transaction != null) {
-        showDialog(
-            context: context,
-            builder: (contextDialog) {
-              return SuccessDialog('successful transaction');
-            }).then((value) => Navigator.pop(context));
-      }
-    }).catchError((e) {
+    final Transaction transaction =
+        await _webClient.save(transactionCreated, password).catchError((e) {
       showDialog(
           context: context,
           builder: (contextDialog) {
             return FailureDialog(e.message);
           });
     }, test: (e) => e is Exception);
+
+    if (transaction != null) {
+      await showDialog(
+          context: context,
+          builder: (contextDialog) {
+            return SuccessDialog('successful transaction');
+          });
+          Navigator.pop(context);
+    }
   }
 }
