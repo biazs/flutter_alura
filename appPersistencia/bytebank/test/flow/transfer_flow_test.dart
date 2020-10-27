7318357fc74b88f9741c5ea491c6f37a908677c5
Bia Zoroastro <fabianazs@gmail.com>
2020-10-27 16:49:22 -0300

    Adicionando comportamentos nos mocks

--- appPersistencia/bytebank/test/flow/transfer_flow_test.dart
+++ appPersistencia/bytebank/test/flow/transfer_flow_test.dart
@@ -1,6 +1,8 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
+import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -18,7 +20,13 @@ void main() {
     final dashboard = find.byType(Dashboard);
     expect(dashboard, findsOneWidget);
 
-       
+    when(mockContactDao.findAll()).thenAnswer((invocation) async {
+      debugPrint('name invocation ${invocation.memberName}');
+      return [Contact(0, 'bia', 1250)];
+    });
+    await clickOnTheTransferFeatureItem(tester);
+    await tester.pumpAndSettle();
+
     await clickOnTheTransferFeatureItem(tester);
     await tester.pumpAndSettle();
 
@@ -27,11 +35,19 @@ void main() {
 
     verify(mockContactDao.findAll()).called(1);
 
+    /* final contactItem = find.byWidgetPredicate((widget) {
+      if (widget is ContactItem) {
+        return widget.contact.name == 'bia' &&
+            widget.contact.accountNumber == 1250;
+      }
+      return false;
+    }); 
 
+    expect(contactItem, findsOneWidget);
+    await tester.tap(contactItem);
+    await tester.pumpAndSettle(); */
 
-
-
-    
-    
+    final transactionForm = find.byType(TransactionForm);
+    expect(transactionForm, findsOneWidget);
   });
 }
