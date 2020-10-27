5b137c495298528dba576c1045a09cfbe3ec47e0
Bia Zoroastro <fabianazs@gmail.com>
2020-10-27 11:47:39 -0300

    Buscando widgets com predicate

--- appPersistencia/bytebank/test/dashboard_widget_test.dart
+++ appPersistencia/bytebank/test/dashboard_widget_test.dart
@@ -2,18 +2,40 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main(){
-  testWidgets('Should display the main image when the Dashboard is opened', (WidgetTester tester) async {
+void main() {
+  testWidgets('Should display the main image when the Dashboard is opened',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
   });
 
-  testWidgets('Should display the transfer feature when the Dashboard is opened', (tester) async {
+  testWidgets(
+      'Should display the transfer feature when the Dashboard is opened',
+      (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+    /* final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
     expect(iconTransferFeatureItem, findsOneWidget);
     final nameTranferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
-    expect(nameTranferFeatureItem, findsOneWidget);
+    expect(nameTranferFeatureItem, findsOneWidget); */
+    final transferFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+    expect(transferFeatureItem, findsOneWidget);
   });
+
+  testWidgets(
+      'Should display the transaction feature when the Dashboard is opened',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final transactionFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+    expect(transactionFeatureItem, findsOneWidget);
+  });
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if (widget is FeatureItem) {
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
