ee99ac1673c2474964ed97aabeccad3dc2a87746
Bia Zoroastro <fabianazs@gmail.com>
2020-10-27 11:25:03 -0300

    Primeiros testes de widgets

--- appPersistencia/bytebank/lib/screens/dashboard.dart
+++ appPersistencia/bytebank/lib/screens/dashboard.dart
@@ -22,12 +22,12 @@ class Dashboard extends StatelessWidget {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                _FeatureItem(
+                FeatureItem(
                   "Transfer",
                   Icons.monetization_on,
                   onClick: () => _showContactsList(context),
                 ),
-                _FeatureItem(
+                FeatureItem(
                   "Transaction Feed",
                   Icons.description,
                   onClick: () => _showTransactionsList(context),
@@ -57,12 +57,12 @@ void _showContactsList(BuildContext context) {
   );
 }
 
-class _FeatureItem extends StatelessWidget {
+class FeatureItem extends StatelessWidget {
   final String name;
   final IconData icon;
   final Function onClick;
 
-  _FeatureItem(this.name, this.icon, {@required this.onClick});
+  FeatureItem(this.name, this.icon, {@required this.onClick});
 
   @override
   Widget build(BuildContext context) {
